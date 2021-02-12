cdf4124cd23cecbcc07288666b822303181c9dfd
Hüseyin Kızılkaya <76448770+huseyinkizilkaya@users.noreply.github.com>
2021-02-13 01:47:08 +0300

    Initial commit

---
+++ Racing_Game/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Racing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnStop = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.btnStartingPosition = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.Location = new System.Drawing.Point(2, 286);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(153, 95);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 22;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(2, 158);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(155, 95);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 21;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 30);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 95);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 20;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnStart
+            // 
+            this.btnStart.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.ForeColor = System.Drawing.Color.Black;
+            this.btnStart.Location = new System.Drawing.Point(12, 429);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(106, 91);
+            this.btnStart.TabIndex = 19;
+            this.btnStart.Text = "START";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label5
+            // 
+            this.label5.BackColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(1258, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(27, 587);
+            this.label5.TabIndex = 18;
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(0, 384);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(1264, 27);
+            this.label4.TabIndex = 17;
+            this.label4.Text = "3 - Turbatur";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(0, 256);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1264, 27);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "2 - Nazlıgül";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(0, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(1264, 27);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "1 - Şahbatur";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1264, 27);
+            this.label1.TabIndex = 14;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnStop
+            // 
+            this.btnStop.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStop.ForeColor = System.Drawing.Color.Black;
+            this.btnStop.Location = new System.Drawing.Point(124, 429);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(106, 91);
+            this.btnStop.TabIndex = 27;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = false;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.White;
+            this.label8.Font = new System.Drawing.Font("Microsoft New Tai Lue", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.Black;
+            this.label8.Location = new System.Drawing.Point(0, 560);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(1264, 27);
+            this.label8.TabIndex = 26;
+            this.label8.Text = "Kızılkaya Race - v1.1";
+            this.label8.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // btnStartingPosition
+            // 
+            this.btnStartingPosition.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnStartingPosition.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStartingPosition.ForeColor = System.Drawing.Color.Black;
+            this.btnStartingPosition.Location = new System.Drawing.Point(236, 429);
+            this.btnStartingPosition.Name = "btnStartingPosition";
+            this.btnStartingPosition.Size = new System.Drawing.Size(106, 91);
+            this.btnStartingPosition.TabIndex = 25;
+            this.btnStartingPosition.Text = "STARTING POSITION";
+            this.btnStartingPosition.UseVisualStyleBackColor = false;
+            this.btnStartingPosition.Click += new System.EventHandler(this.btnStartingPosition_Click);
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.label7.Font = new System.Drawing.Font("Microsoft New Tai Lue", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(398, 429);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(106, 91);
+            this.label7.TabIndex = 24;
+            this.label7.Text = "0";
+            this.label7.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.BackColor = System.Drawing.Color.Silver;
+            this.label6.Font = new System.Drawing.Font("Palatino Linotype", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.Color.Black;
+            this.label6.Location = new System.Drawing.Point(577, 423);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(661, 125);
+            this.label6.TabIndex = 23;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1553, 611);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btnStartingPosition);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button btnStartingPosition;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Racing_Game/Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Racing_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        int firsthorsedistance, secondhorsedistance, thirdhorsedistance;    //distance to the left
+        public int counter = 0;
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+        }
+        private void btnStartingPosition_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Left = 0;
+            pictureBox2.Left = 0;
+            pictureBox3.Left = 0;
+            counter = 0;
+            label7.Text = "0";
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            counter = Convert.ToInt32(label7.Text);
+            counter++;
+            label7.Text = counter.ToString();
+
+            int firsthoursewidth = pictureBox1.Width;
+            int secondhorsewidth = pictureBox2.Width;
+            int thirdhorsewidth = pictureBox3.Width;
+            int finishingline = label5.Left;
+
+            Random random = new Random();
+            pictureBox1.Left = pictureBox1.Left + random.Next(3, 25);
+            pictureBox2.Left = pictureBox2.Left + random.Next(3, 25);
+            pictureBox3.Left = pictureBox3.Left + random.Next(3, 25);
+
+            if (pictureBox1.Left > pictureBox2.Left + 5 && pictureBox1.Left > pictureBox3.Left + 5)
+            {
+                label6.Text = "The horse number '1' is front-running...";
+            }
+            if (pictureBox2.Left > pictureBox1.Left + 5 && pictureBox2.Left > pictureBox3.Left + 5)
+            {
+                label6.Text = "The horse number '2' getting on a sudden sprint...";
+            }
+            if (pictureBox3.Left > pictureBox1.Left + 5 && pictureBox3.Left > pictureBox2.Left + 5)
+            {
+                label6.Text = "The horse number '3' keep the leading position...";
+            }
+
+            if (firsthoursewidth + pictureBox1.Left >= finishingline)
+            {
+                timer1.Enabled = false;
+
+                if (secondhorsewidth + pictureBox2.Left >= thirdhorsewidth + pictureBox3.Left)
+                {
+                    label6.Text = "- OMG, The horse number '1' won !!!" +
+                                  "\n- The horse number '2' is runner up" +
+                                  "\n- The horse number '3' is come in third in the race";
+                }
+                if (thirdhorsewidth + pictureBox3.Left >= secondhorsewidth + pictureBox2.Left)
+                {
+                    label6.Text = "- OMG, The horse number '1' won !!!" +
+                                  "\n- The horse number '3' is runner up" +
+                                  "\n- The horse number '2' is come in third in the race";
+                }
+            }
+            if (secondhorsewidth + pictureBox2.Left >= finishingline)
+            {
+                timer1.Enabled = false;
+
+                if (firsthoursewidth + pictureBox1.Left >= thirdhorsewidth + pictureBox3.Left)
+                {
+                    label6.Text = "- OMG, The horse number '2' won !!!" +
+                                  "\n- The horse number '1' is runner up" +
+                                  "\n- The horse number '3' is come in third in the race";
+                }
+                if (thirdhorsewidth + pictureBox3.Left >= firsthoursewidth + pictureBox1.Left)
+                {
+                    label6.Text = "- OMG, The horse number '2' won !!!" +
+                                  "\n- The horse number '3' is runner up" +
+                                  "\n- The horse number '1' is come in third in the race";
+                }
+            }
+            if (thirdhorsewidth + pictureBox3.Left >= finishingline)
+            {
+                timer1.Enabled = false;
+
+                if (firsthoursewidth + pictureBox1.Left >= secondhorsewidth + pictureBox2.Left)
+                {
+                    label6.Text = "- OMG, The horse number '3' won !!!" +
+                                  "\n- The horse number '1' is runner up" +
+                                  "\n- The horse number '2' is come in third in the race";
+                }
+                if (secondhorsewidth + pictureBox2.Left >= firsthoursewidth + pictureBox1.Left)
+                {
+                    label6.Text = "- OMG, The horse number '3' won !!!" +
+                                  "\n- The horse number '2' is runner up" +
+                                  "\n- The horse number '1' is come in third in the race";
+                }
+            }
+        }   
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            firsthorsedistance = pictureBox1.Left;
+            secondhorsedistance = pictureBox2.Left;
+            thirdhorsedistance = pictureBox3.Left;
+        }
+    }
+}
